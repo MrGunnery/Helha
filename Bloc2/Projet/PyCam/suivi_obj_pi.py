a9eec184c4773db3d87e737d57adbd5ad6d6def2
Martin <martin@localhost.localdomain>
2018-02-23 18:21:36 +0100

    gs

--- Bloc2/Projet/PyCam/suivi_obj_pi.py
+++ Bloc2/Projet/PyCam/suivi_obj_pi.py
@@ -26,36 +26,26 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
          image = frame.array
          # convertir l'image en gris
          hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-
-	     # construct a mask for the color "green", then perform
-	     # a series of dilations and erosions to remove any small
-    	 # blobs left in the mask
-	     mask = cv2.inRange(hsv, colorLower, colorUpper)
-	     mask = cv2.erode(mask, None, iterations=2)
-	     mask = cv2.dilate(mask, None, iterations=2)
+         
+         mask = cv2.inRange(hsv, colorLower, colorUpper)
+         mask = cv2.erode(mask, None, iterations=2)
+         mask = cv2.dilate(mask, None, iterations=2)
+    
          # find contours in the mask and initialize the current
-     	 # (x, y) center of the ball
-     	 cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-     		 cv2.CHAIN_APPROX_SIMPLE)[-2]
-     	 center = None
-
-     	 # only proceed if at least one contour was found
-     	 if len(cnts) > 0:
-     		 # find the largest contour in the mask, then use
-     		 # it to compute the minimum enclosing circle and
-     		 # centroid
-     		 c = max(cnts, key=cv2.contourArea)
-     		 ((x, y), radius) = cv2.minEnclosingCircle(c)
-     		 M = cv2.moments(c)
-     		 center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-
-     		 # only proceed if the radius meets a minimum size
-     		 if radius > 10:
-     			 # draw the circle and centroid on the frame,
-     			 # then update the list of tracked points
-     			 cv2.circle(image, (int(x), int(y)), int(radius),
-     				 (0, 255, 255), 2)
-     			 cv2.circle(image, center, 5, (0, 0, 255), -1)
+         # (x, y) center of the ball
+         cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                                 cv2.CHAIN_APPROX_SIMPLE)[-2]
+         center = None
+         # only proceed if at least one contour was found
+         if len(cnts) > 0:
+             c = max(cnts, key=cv2.contourArea)
+             ((x, y), radius) = cv2.minEnclosingCircle(c)
+             M=cv2.moments(c)
+             center= (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+             if radius > 10:
+                 cv2.circle(image, (int(x), int(y)), int(radius),
+                            (0, 255, 255), 2)
+                 cv2.circle(image, center, 5, (0, 0, 255), -1)
 
          # affiche les differentes frames
          cv2.imshow("Fraeme", image)
