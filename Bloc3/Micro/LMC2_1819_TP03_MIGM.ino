0420c13133be302bfb3a22b7efa4223022aa2ded
Martin <34959687+MrGunnery@users.noreply.github.com>
2018-10-19 18:51:17 +0200

    Add files via upload

---
+++ Bloc3/Micro/LMC2_1819_TP03_MIGM.ino
@@ -0,0 +1,34 @@
+#include "Arduino.h"
+#include "MultiFuncShield.h"
+#include "TimerOne.h"
+#include "Wire.h"
+
+/**
+ * @author Martin
+ */
+
+/**
+ * Fonction Setup
+ */
+void setup(){
+	Timer1.initialize();
+	MFS.initialize(&Timer1);
+	Serial.begin(9600);
+	MFS.write(8888);
+}
+
+unsigned long previousMillis = 0;
+const long interval = 1000*60*3;
+
+/**
+ * Fonction loop
+ */
+void loop() {
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    int nbr = map(analogRead(0), 0, 1023, 0, 100);
+    MFS.write(nbr);
+  }
+}
