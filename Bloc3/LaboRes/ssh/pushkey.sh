738e9431c633372c1ec5f6d5c79478a31d58c390
Emzelim <34270611+Emzelim@users.noreply.github.com>
2018-11-20 17:36:52 +0100

    Update pushkey.sh

--- Bloc3/LaboRes/ssh/pushkey.sh
+++ Bloc3/LaboRes/ssh/pushkey.sh
@@ -3,14 +3,15 @@
 PUBKEYDIR="/root/.ssh"
 LOGDIR="/root/admin/logs"
 IPFILE="/root/admin/ip.txt"
+NAMELOG="basename $0"
 
 for IP in 'cat $IPFILE'
 do
   if ping -c 2 $IP > /dev/null 2>&1
   then
     ssh-copy-id root@$IP > /dev/null 2>&1
-    echo "KPub recopiee sur $IP .... OK" >> $LOGDIR/pushkey.sh.log
+    echo "KPub recopiee sur $IP .... OK" >> $LOGDIR/$NAMELOG.log
   else
-    echo "$IP ne repond pas..." >> $LOGDIR/pushkey.sh.error.log
+    echo "$IP ne repond pas..." >> $LOGDIR/$NAMELOG.error.log
   fi
 done
