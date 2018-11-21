173854e992cd7f78b277cf21ef9abf91ea88230b
Emzelim <34270611+Emzelim@users.noreply.github.com>
2018-11-21 14:28:44 +0100

    Create alladduser.sh

---
+++ Bloc3/LaboRes/ssh/exe4/alladduser.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+. /root/admin/sh/initvar.sh
+
+for IP in 'cat $IPFILE'
+do
+  if ping -c 2 $IP > /dev/null 2>&1
+  then
+    ssh root@$IP "echo $1 | passwd --stdin root" > /dev/null 2>&1
+    echo "MdP sur $IP changee .... OK" >> $LOGDIR/$NAMELOG.log
+  else
+    echo "$IP ne repond pas..." >> $LOGDIR/$NAMELOG.error.log
+  fi
+done
