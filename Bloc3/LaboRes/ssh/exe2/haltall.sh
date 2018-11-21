1a3be73cab47e352527baf385587212d0eb8a6d7
Emzelim <34270611+Emzelim@users.noreply.github.com>
2018-11-21 14:23:31 +0100

    Update haltall.sh

--- Bloc3/LaboRes/ssh/exe2/haltall.sh
+++ Bloc3/LaboRes/ssh/exe2/haltall.sh
@@ -6,7 +6,7 @@ for IP in 'cat $IPFILE'
 do
   if ping -c 2 $IP > /dev/null 2>&1
   then
-    ssh root$IP "shutdown -h now"
+    ssh root@$IP "shutdown -h now"
     echo "Extinction de $IP .... OK" >> $LOGDIR/$NAMELOG.log
   else
     echo "$IP eteinte..." >> $LOGDIR/$NAMELOG.error.log
