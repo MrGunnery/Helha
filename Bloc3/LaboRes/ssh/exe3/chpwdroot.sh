16e88d9ca58c14777d74ea9c2cb9057fa7fd3c2f
Emzelim <34270611+Emzelim@users.noreply.github.com>
2018-11-21 14:17:21 +0100

    Create chpwdroot.sh

---
+++ Bloc3/LaboRes/ssh/exe3/chpwdroot.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+. /root/admin/sh/initvar.sh
+
+for IP in 'cat $IPFILE'
+do
+  if ping -c 2 $IP > /dev/null 2>&1
+  then
+    ssh-copy-id root@$IP > /dev/null 2>&1
+    echo "KPub recopiee sur $IP .... OK" >> $LOGDIR/$NAMELOG.log
+  else
+    echo "$IP ne repond pas..." >> $LOGDIR/$NAMELOG.error.log
+  fi
+done
