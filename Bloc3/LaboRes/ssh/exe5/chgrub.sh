ab1e6c0ed0a2d925810c91912d4a84b5a1646abf
Emzelim <34270611+Emzelim@users.noreply.github.com>
2018-11-21 14:55:04 +0100

    Update chgrub.sh

--- Bloc3/LaboRes/ssh/exe5/chgrub.sh
+++ Bloc3/LaboRes/ssh/exe5/chgrub.sh
@@ -6,9 +6,9 @@ for IP in 'cat $IPFILE'
 do
   if ping -c 2 $IP > /dev/null 2>&1
   then
-    ssh root@$IP "useradd $1 > /dev/null 2>&1"
-    ssh root@$IP "echo $1 | passwd --stdin $1 > /dev/null 2>&1"
-    echo "Ajout de $1 sur $IP changee .... OK" >> $LOGDIR/$NAMELOG.log
+    ssh root@$IP "sed -i -e \"s/TIMEOUT=0/TIMEOUT=5/g\" /etc/default/grub "
+    ssh root@$IP "grub2-mkconfig -o /boot/grub2/grub.cfg"
+    echo "Modif de grub sur $IP changee .... OK" >> $LOGDIR/$NAMELOG.log
   else
     echo "$IP ne repond pas..." >> $LOGDIR/$NAMELOG.error.log
   fi
