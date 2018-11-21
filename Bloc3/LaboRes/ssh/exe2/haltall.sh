4cfa1637d7a389162abd537d01a90177ab8bdaa2
Emzelim <34270611+Emzelim@users.noreply.github.com>
2018-11-21 13:56:46 +0100

    Update haltall.sh

--- Bloc3/LaboRes/ssh/exe2/haltall.sh
+++ Bloc3/LaboRes/ssh/exe2/haltall.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
 
+. /root/admin/sh/initvar.sh
+
+for IP in 'cat $IPFILE'
+do
+  if ping -c 2 $IP > /dev/null 2>&1
+  then
+    ssh-copy-id root@$IP > /dev/null 2>&1
+    echo "Extinction de $IP .... OK" >> $LOGDIR/$NAMELOG.log
+  else
+    echo "$IP eteinte..." >> $LOGDIR/$NAMELOG.error.log
+  fi
+done
