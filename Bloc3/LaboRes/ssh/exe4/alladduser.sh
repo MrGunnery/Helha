352bd29f6f5923cc006e7dc9920d844046a2df78
Emzelim <34270611+Emzelim@users.noreply.github.com>
2018-11-21 14:35:18 +0100

    Update alladduser.sh

--- Bloc3/LaboRes/ssh/exe4/alladduser.sh
+++ Bloc3/LaboRes/ssh/exe4/alladduser.sh
@@ -6,8 +6,9 @@ for IP in 'cat $IPFILE'
 do
   if ping -c 2 $IP > /dev/null 2>&1
   then
-    ssh root@$IP "echo $1 | passwd --stdin root" > /dev/null 2>&1
-    echo "MdP sur $IP changee .... OK" >> $LOGDIR/$NAMELOG.log
+    ssh root@$IP "useradd $1 > /dev/null 2>&1"
+    ssh root@$IP "echo $1 | passwd --stdin $1 > /dev/null 2>&1"
+    echo "Ajout de $1 sur $IP changee .... OK" >> $LOGDIR/$NAMELOG.log
   else
     echo "$IP ne repond pas..." >> $LOGDIR/$NAMELOG.error.log
   fi
