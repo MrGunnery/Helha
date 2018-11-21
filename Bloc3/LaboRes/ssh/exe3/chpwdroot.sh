c7456b4585059ebbd9bb0df80116dc32bdcd18ec
Emzelim <34270611+Emzelim@users.noreply.github.com>
2018-11-21 14:23:05 +0100

    Update chpwdroot.sh

--- Bloc3/LaboRes/ssh/exe3/chpwdroot.sh
+++ Bloc3/LaboRes/ssh/exe3/chpwdroot.sh
@@ -6,8 +6,8 @@ for IP in 'cat $IPFILE'
 do
   if ping -c 2 $IP > /dev/null 2>&1
   then
-    ssh-copy-id root@$IP > /dev/null 2>&1
-    echo "KPub recopiee sur $IP .... OK" >> $LOGDIR/$NAMELOG.log
+    ssh root@$IP "echo $1 | passwd --stdin root"
+    echo "MdP sur $IP changee .... OK" >> $LOGDIR/$NAMELOG.log
   else
     echo "$IP ne repond pas..." >> $LOGDIR/$NAMELOG.error.log
   fi
